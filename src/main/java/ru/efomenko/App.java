c802ebf7cf8526599653aecf235cb9c079a2d43a
Evgeniy Fomenko <49038282+EvgeniyFomenko@users.noreply.github.com>
2024-01-17 23:58:55 +0500

    Comit init

--- src/main/java/ru/efomenko/App.java
+++ src/main/java/ru/efomenko/App.java
@@ -6,17 +6,15 @@ import ru.efomenko.util.JsonTicketsConverter;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
+public class App {
     private final static String pathToFile = "src/main/resources/tickets.json";
-    public static void main( String[] args ) throws IOException {
-        CalculationService calculationService = new CalculationService( JsonTicketsConverter.getTicketsFromJson(FileWorker.getContent(Path.of(pathToFile))));
+
+    public static void main(String[] args) throws IOException {
+        CalculationService calculationService = new CalculationService(JsonTicketsConverter.getTicketsFromJson(FileWorker.getContent(Path.of(pathToFile))));
         calculationService.calculateMinimalTimeFlight();
         calculationService.calculateMedianDifferenceAveragePrice();
     }
