2b25d10d532d4f1be8daf719a9eb97da92ed6c87
Evgeniy Fomenko <49038282+EvgeniyFomenko@users.noreply.github.com>
2024-01-17 23:57:11 +0500

    Comit init

--- src/main/java/ru/efomenko/util/FileWorker.java
+++ src/main/java/ru/efomenko/util/FileWorker.java
@@ -1,17 +1,20 @@
 package ru.efomenko.util;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.nio.file.Path;
 
 public class FileWorker {
-    public static String getContent(Path path) throws IOException {
+    public static String getContent(Path path) {
         File file = new File(path.toUri());
         StringBuilder stringBuilder = new StringBuilder();
 
         try (Reader fileReader = new FileReader(file)) {
             int data = fileReader.read();
             while (data != -1) {
-                stringBuilder.append((char)data);
+                stringBuilder.append((char) data);
                 data = fileReader.read();
             }
         } catch (IOException ioException) {
--- src/main/java/ru/efomenko/util/JsonTicketsConverter.java
+++ src/main/java/ru/efomenko/util/JsonTicketsConverter.java
@@ -1,31 +1,24 @@
 package ru.efomenko.util;
 
 import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
 import ru.efomenko.pojo.Ticket;
 import ru.efomenko.util.adapter.DateAdapter;
 import ru.efomenko.util.adapter.TimeAdapter;
 
-
-import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class JsonTicketsConverter {
     public static List<Ticket> getTicketsFromJson(String jsonTicket) {
         GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(LocalDate.class,new DateAdapter())
-                .registerTypeAdapter(LocalTime.class,new TimeAdapter());
-        Gson gson =gsonBuilder.create();
+        Gson gson = gsonBuilder.registerTypeAdapter(LocalDate.class, new DateAdapter())
+                .registerTypeAdapter(LocalTime.class, new TimeAdapter()).create();
         JsonElement jsonElement = JsonParser.parseString(jsonTicket);
         JsonObject jsonObject = jsonElement.getAsJsonObject();
-//        Ticket[] tickets =  ;
-//        Type targetClassType = new TypeToken<List<Ticket>>(){}.getType();
-        List<Ticket> targetCollection =  Arrays.asList(gson.fromJson(jsonObject.getAsJsonArray("tickets"), Ticket[].class));
+        List<Ticket> targetCollection = Arrays.asList(gson.fromJson(jsonObject.getAsJsonArray("tickets"), Ticket[].class));
+
         return targetCollection;
     }
 }
