8360fb84d380fcb17d3cdbc424fb8e886de879ca
Evgeniy Fomenko <49038282+EvgeniyFomenko@users.noreply.github.com>
2024-01-19 18:15:55 +0500

    fix: поправил логику расчёта разницы

--- src/main/java/ru/efomenko/App.java
+++ src/main/java/ru/efomenko/App.java
@@ -8,9 +8,11 @@ import java.nio.file.Path;
 
 public class App {
     private final static String pathToFile = "src/main/resources/tickets.json";
+    private final static String from = "VVO";
+    private final static String to = "TLV";
 
     public static void main(String[] args) {
-        CalculationService calculationService = new CalculationService(JsonTicketsConverter.getTicketsFromJson(FileWorker.getContent(Path.of(pathToFile))));
+        CalculationService calculationService = new CalculationService(JsonTicketsConverter.getTicketsFromJson(FileWorker.getContent(Path.of(pathToFile))), from, to);
         calculationService.calculateMinimalTimeFlight();
         calculationService.calculateMedianDifferenceAveragePrice();
     }
--- src/main/java/ru/efomenko/service/CalculationService.java
+++ src/main/java/ru/efomenko/service/CalculationService.java
@@ -13,48 +13,72 @@ import java.util.stream.Collectors;
 
 public class CalculationService {
     private final List<Ticket> ticketList;
-    private final String from = "VVO";
-    private final String to = "TLV";
+    private final String from ;
+    private final String to;
 
-    public CalculationService(List<Ticket> ticketList) {
+    public CalculationService(List<Ticket> ticketList, String from, String to) {
+        this.from = from;
+        this.to = to;
         this.ticketList = ticketList;
     }
 
     public void calculateMedianDifferenceAveragePrice() {
-
-        AtomicInteger sum = new AtomicInteger();
-        if(ticketList.isEmpty()){
+        if (ticketList.isEmpty()) {
             return;
         }
-        List<Ticket> sortTickets = ticketList.stream().filter(ticket -> ticket.getOrigin().equals(from) && ticket.getDestination().equals(to))
-                .sorted(Comparator.comparingInt(Ticket::getPrice)).collect(Collectors.toList());
-        int averageNumber = 2;
-        int medinePrice = sortTickets.get(((sortTickets.size()-1) / averageNumber)).getPrice();
 
+        List<Ticket> sortedTicketList = ticketList.stream()
+                .filter(this::isaRightRoute)
+                .sorted(Comparator.comparing(Ticket::getPrice))
+                .collect(Collectors.toList());
+        int result = getMedineAverage(sortedTicketList);
+
+        System.out.println(String.format("Разница медины и средней цены из %s в %s составляет %d", from, to, result));
+    }
+
+    private boolean isaRightRoute(Ticket ticket) {
+        return ticket.getOrigin().equals(from) && ticket.getDestination().equals(to);
+    }
+
+
+    private int getMedineAverage(List<Ticket> sortTickets) {
+        int medinePrice = getMedianPrice(sortTickets);
+        int averagePrice = getAveragePrice(sortTickets);
+        return medinePrice - averagePrice;
+    }
+
+    private static int getAveragePrice(List<Ticket> sortTickets) {
+        AtomicInteger sum = new AtomicInteger();
         sortTickets.forEach(ticket -> sum.addAndGet(ticket.getPrice()));
+        int countTickets = sortTickets.size();
+        return sum.get() / countTickets;
+    }
 
-        long countTickets = sortTickets.size();
-        long averagePrice = sum.get() / countTickets;
-        System.out.println(String.format("Разница медины и средней цены из %s в %s составляет %d", from, to,Math.abs(medinePrice - averagePrice) ));
-
-
+    private static int getMedianPrice(List<Ticket> sortTickets) {
+        int averageNumber = 2;
+        if (sortTickets.size() % 2 == 0) {
+            int oneMedianNumb = (sortTickets.size() - 1) / averageNumber;
+            int twoMedianNumb = oneMedianNumb + 1;
+            return (sortTickets.get(oneMedianNumb).getPrice() + sortTickets.get(twoMedianNumb).getPrice()) / 2;
+        }
+        return sortTickets.get(((sortTickets.size() - 1) / averageNumber)).getPrice();
     }
 
     public void calculateMinimalTimeFlight() {
         Map<String, Duration> shortTimeArrived = new HashMap<>();
-        if(ticketList.isEmpty()){
+        if (ticketList.isEmpty()) {
             return;
         }
-        ticketList.stream().filter(ticket -> ticket.getOrigin().equals(from) && ticket.getDestination().equals(to))
+        ticketList.stream().filter(this::isaRightRoute)
                 .peek(ticket -> shortTimeArrived.putIfAbsent(ticket.getCarrier(), getDurationTicket(ticket)))
                 .forEach(ticket -> shortTimeArrived.put(ticket.getCarrier(), getShortDuration(getDurationTicket(ticket), shortTimeArrived.get(ticket.getCarrier()))));
 
-        shortTimeArrived.keySet().forEach(key -> printResult(key, shortTimeArrived, from, to));
+        shortTimeArrived.keySet().forEach(key -> printResult(key, shortTimeArrived));
     }
 
-    private static void printResult(String car, Map<String, Duration> shortTimeArrived, String from, String to) {
-        Duration duration = shortTimeArrived.get(car);
-        System.out.println(String.format("Продолжительность самого короткого поерелёта из %s в %s перевозчика %s составила %d часов %d минут", from, to, car, duration.toHoursPart(), duration.toMinutesPart()));
+    private void printResult(String carrier, Map<String, Duration> shortTimeArrived) {
+        Duration duration = shortTimeArrived.get(carrier);
+        System.out.println(String.format("Продолжительность самого короткого поерелёта из %s в %s перевозчика %s составила %d часов %d минут", from, to, carrier, duration.toHoursPart(), duration.toMinutesPart()));
     }
 
     private Duration getDurationTicket(Ticket ticket) {
